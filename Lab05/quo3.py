17b2625fe4abce3ef5f44343e22a9694cb2a1be0
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2025-09-22 13:53:53 +0530

    Master Update

--- Lab05/quo3.py
+++ Lab05/quo3.py
@@ -1,3 +1,10 @@
+# Design a Python-based experiment to analyze the performance of MD5, SHA-1, and
+# SHA-256 hashing techniques in terms of computation time and collision resistance.
+# Generate a dataset of random strings ranging from 50 to 100 strings, compute the hash
+# values using each hashing technique, and measure the time taken for hash computation.
+# Implement collision detection algorithms to identify any collisions within the hashed dataset
+
+
 import hashlib
 import random
 import string
